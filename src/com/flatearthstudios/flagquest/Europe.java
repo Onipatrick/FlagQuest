3401035521e7f5e3d250c6c731c88403c61265f7
Patrick H. <iamstillawesome1@gmail.com>
2013-03-03 23:17:33 -0500

    Beginning to implement Europe

--- src/com/flatearthstudios/flagquest/Europe.java
+++ src/com/flatearthstudios/flagquest/Europe.java
@@ -3,9 +3,15 @@ package com.flatearthstudios.flagquest;
 import java.util.Random;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.res.Resources;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Europe extends Activity {
 
@@ -13,23 +19,63 @@ public class Europe extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.europe);
         
-      //Initialize Random
-    	Random rand;
-    	//Define minimum and maximum values
-    	int min = 0;
-    	int max = 9;
-    	//Initialize a variable to store random
-    	rand = new Random();
-    	//Turns random into the next integer within the minimum and maximum values
-    	int randomNum = rand.nextInt(max - min + 1) + min;
-        //Accesses Text View by the ID    
+      //Accesses Text View by the ID    
         final TextView setTextView = ((TextView) findViewById (R.id.txtEurope));
         Resources res = getResources();
         //Retrieves string array from strings.xml and stores it in the variable 'country'
         final String[] country = res.getStringArray(R.array.europe_country_names);
         //The country variable can now declare any item in the array by indicating its position
-        setTextView.setText(country[randomNum]);
+        setTextView.setText(country[0]);
         
-	};
-	
+        
+        final MediaPlayer mpCorrect;
+		final MediaPlayer mpIncorrect;
+        mpCorrect = MediaPlayer.create(this, R.raw.correct);
+        mpIncorrect = MediaPlayer.create(this, R.raw.incorrect);
+        
+        //If flag 1
+    	ImageView imgBtnNorth = (ImageView) findViewById(R.id.flag1_1);
+		imgBtnNorth.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				
+				mpIncorrect.start();
+				Toast.makeText(Europe.this, "Incorrect, please try again.", Toast.LENGTH_SHORT).show();
+			}
+
+        	
+        });
+
+		//If flag 2
+		ImageView imgBtnSouth = (ImageView) findViewById(R.id.flag1_2);
+		imgBtnSouth.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				mpCorrect.start();    				
+				startActivity(new Intent(Europe.this, Europe2.class));
+			}
+
+        	
+        });
+
+		//If flag 3
+		ImageView imgBtnEurope = (ImageView) findViewById(R.id.flag1_3);
+		imgBtnEurope.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				mpIncorrect.start();
+				Toast.makeText(Europe.this, "Incorrect, please try again.", Toast.LENGTH_SHORT).show();
+			}
+
+        	
+        });
+
+
+
+};
+
 }//End Europe Activity
+
