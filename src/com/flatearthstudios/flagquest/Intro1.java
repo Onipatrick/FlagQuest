ab57961b274a2390f369d4cd4211f5d2058d6545
Patrick H. <iamstillawesome1@gmail.com>
2013-03-03 19:54:47 -0500

    Everything working preparing for splash

---
+++ src/com/flatearthstudios/flagquest/Intro1.java
@@ -0,0 +1,33 @@
+package com.flatearthstudios.flagquest;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Intro1 extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.intro1);
+		
+		TimerTask task = new TimerTask(){
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				finish();
+				startActivity(new Intent(Intro1.this, Main.class));
+			}
+		
+	};
+	
+	Timer opening = new Timer();
+	opening.schedule(task,5000);
+
+}
+	
+}
